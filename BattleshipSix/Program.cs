1d3a2a41b4314cb0959faf2d8ee6b1b0e85f4af8
Tommy Wu <tommy.wu2@education.nsw.gov.au>
2023-04-04 14:11:21 +1000

    Add project files.

---
+++ BattleshipSix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipSix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string[,] playerBoardOne = new string[10, 10];
+            string[,] playerBoardTwo = new string[10, 10];
+            bool versusAI = false;
+            string currentPlayer = null;
+            string boatRotation = null;
+            int boatCoordinateX = 0;
+            int boatCoordinateY = 0;
+            int boardCounter = 0;
+
+            CreateBoard(playerBoardOne, playerBoardTwo, boardCounter, ref currentPlayer);
+        }
+        
+        //makes your own board where you were hit
+        private static void CreateBoard(string[,] playerBoardOne, string[,] playerBoardTwo, int boardCounter, ref string currentPlayer)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                boardCounter = i + 1;
+                Console.Write("   " + boardCounter + "   ");
+            }
+
+            if (currentPlayer == "one")
+            {
+                
+                
+            }
+            
+
+        }
+    }
+}
