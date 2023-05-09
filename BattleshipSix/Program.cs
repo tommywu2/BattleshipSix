c7743293077d4f7f6afd2a290ad3e0100c67f837
Tommy Wu <tommy.wu2@education.nsw.gov.au>
2023-05-09 14:12:18 +1000

    wow update for once

--- BattleshipSix/Program.cs
+++ BattleshipSix/Program.cs
@@ -15,9 +15,12 @@ namespace BattleshipSix
             int boatCoordinateX = 0;
             int boatCoordinateY = 0;
             int boardCounter = 0;
+            int xCoordShot = 0;
+            int yCoordShot = 0;
 
-            playerBoardOne[9, 3] = "x";
+
             CreateBoard(playerBoardOne, playerBoardTwo, boardCounter, ref currentPlayer);
+            PlayerTurn(playerBoardOne, playerBoardTwo, boardCounter, ref currentPlayer, ref xCoordShot, ref yCoordShot);
         }
         
         //makes your own board where you were hit
@@ -33,9 +36,19 @@ namespace BattleshipSix
             //check if play is one
             if (currentPlayer == "one")
             {
-                //draws the top part of the square
+                //draws the horizontal line
+                Console.WriteLine();
+                Console.Write("     ");
+                for (int m = 0; m < 10; m++)
+                {
+                    Console.Write(" ******");
+                }
+                //draws the player's board
                 for (int i = 0; i < 10; i++)
                 {
+                    //sets draw counter to i + 1
+                    boardCounter = i + 1;
+                    //draws the top part of the square
                     Console.WriteLine();
                     Console.Write("     ");
                     for (int j = 0; j < 10; j++)
@@ -46,7 +59,7 @@ namespace BattleshipSix
 
                     //draws the middle with the player's board
                     Console.WriteLine();
-                    Console.Write("     ");
+                    Console.Write("  " + boardCounter + "  ");
                     for (int k = 0; k < 10; k++)
                     {
                         Console.Write("|   " + playerBoardOne[i, k] + "   ");
@@ -70,7 +83,95 @@ namespace BattleshipSix
                         Console.Write(" ******");
                     }
                 }
-            } 
+            }
+            //check if play is two
+            if (currentPlayer == "two")
+            {
+                //draws the horizontal line
+                Console.WriteLine();
+                Console.Write("     ");
+                for (int m = 0; m < 10; m++)
+                {
+                    Console.Write(" ******");
+                }
+                //draws the player's board
+                for (int i = 0; i < 10; i++)
+                {
+                    //sets draw counter to i + 1
+                    boardCounter = i + 1;
+                    //draws the top part of the square
+                    Console.WriteLine();
+                    Console.Write("  " + boardCounter + "  ");
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.Write("|      ");
+                    }
+                    Console.Write("|");
+
+                    //draws the middle with the player's board
+                    Console.WriteLine();
+                    Console.Write("     ");
+                    for (int k = 0; k < 10; k++)
+                    {
+                        Console.Write("|   " + playerBoardTwo[i, k] + "   ");
+                    }
+                    Console.Write("|");
+
+                    //draws the bottom of the square
+                    Console.WriteLine();
+                    Console.Write("     ");
+                    for (int l = 0; l < 10; l++)
+                    {
+                        Console.Write("|      ");
+                    }
+                    Console.Write("|");
+
+                    //draws the horizontal line
+                    Console.WriteLine();
+                    Console.Write("     ");
+                    for (int m = 0; m < 10; m++)
+                    {
+                        Console.Write(" ******");
+                    }
+                }
+            }
+        }
+        private static void PlayerTurn(string[,] playerBoardOne, string[,] playerBoardTwo, int boardCounter, ref string currentPlayer, ref int xCoordShot, ref int yCoordShot)
+        {
+            //shows which player turn
+            Console.WriteLine("\nCurrent Player: " + currentPlayer);
+            if (currentPlayer == "one")
+            {
+                Console.WriteLine("What x coordinate for shot");
+                xCoordShot = Convert.ToInt32(Console.ReadLine());
+                //checks if bigger than 0
+                if (xCoordShot > 0)
+                {
+                    //checks if smaller than 11
+                    if (xCoordShot < 11)
+                    {
+                        Console.WriteLine("What y coordinate for shot");
+                        yCoordShot = Convert.ToInt32(Console.ReadLine());
+                        //checks if bigger than 0
+                        if (yCoordShot > 0)
+                        {
+                            //checks if smaller than 11
+                            if (yCoordShot < 11)
+                            {
+
+                            }
+                            else PlayerTurn(playerBoardOne, playerBoardTwo, boardCounter, ref currentPlayer, ref xCoordShot, ref yCoordShot);
+                        }
+                        else PlayerTurn(playerBoardOne, playerBoardTwo, boardCounter, ref currentPlayer, ref xCoordShot, ref yCoordShot);
+                    }
+                    else PlayerTurn(playerBoardOne, playerBoardTwo, boardCounter, ref currentPlayer, ref xCoordShot, ref yCoordShot);
+                }
+                else PlayerTurn(playerBoardOne, playerBoardTwo, boardCounter, ref currentPlayer, ref xCoordShot, ref yCoordShot);
+            }
+        }
+        private static void BoatSetup()
+        {
+
         }
     }
 }
